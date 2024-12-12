2c7a60ae443da1ddc9186db9fe5dd7ff8a23be97
rakib544 <md.rakib10122003@gmail.com>
2024-12-12 09:20:31 +0600

    feat:implemented lesson add and edit form in the dashboard lessons page

---
+++ src/app/dashboard/lessons/add/lesson-add-form.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const FormSchema = z.object({
+  title: z.string().trim().min(1, { message: "Title is required" }),
+  lessonNo: z.coerce.number().int().gt(0),
+});
+export default function LessonAddForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {},
+  });
+  type FormValues = z.infer<typeof FormSchema>;
+
+  async function onSubmit(data: FormValues) {
+    console.log(data);
+  }
+
+  return (
+    <div className="max-w-xl mt-12">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="space-y-6">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input
+                      autoFocus
+                      {...field}
+                      placeholder="e.g. Introduction"
+                      className=""
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="lessonNo"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Lesson No</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="number"
+                      placeholder="e.g. 1"
+                      className=""
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div className="flex justify-end mt-8">
+            <Button type="submit">Add lesson</Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+}
---
+++ src/app/dashboard/lessons/page.tsx
@@ -0,0 +1,17 @@
+import { CirclePlus } from "@/components/icons";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default async function DashboardTutorialsPage() {
+  return (
+    <div>
+      <div className="flex justify-end">
+        <Button asChild>
+          <Link href="/dashboard/lessons/add">
+            Add New <CirclePlus className="h-[18px] w-[18px] ml-1" />
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
+}
