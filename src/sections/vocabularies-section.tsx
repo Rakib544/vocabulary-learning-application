3e5c32391be4e6e2dfbf760faf40999a89d71b5b
rakib544 <md.rakib10122003@gmail.com>
2024-12-12 23:10:42 +0600

    feat:implemented react-confetti to display lesson completion celebration

--- src/sections/vocabularies-section.tsx
+++ src/sections/vocabularies-section.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { Volume2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import Confetti from "react-confetti";
+import { useWindowSize } from "usehooks-ts";
 
 interface VocabulariesInterface {
   word: string;
@@ -16,6 +20,25 @@ export default function VocabulariesSection({
   vocabularies: VocabulariesInterface[];
 }) {
   const [currentItem, setCurrentItem] = useState(0);
+  const { width = 0, height = 0 } = useWindowSize();
+  const [showConfetti, setShowConfetti] = useState(false);
+  const router = useRouter();
+
+  function pronounceWord(word: string) {
+    const utterance = new SpeechSynthesisUtterance(word);
+    utterance.lang = "ja-JP"; // Japanese
+    window.speechSynthesis.speak(utterance);
+  }
+
+  useEffect(() => {
+    let timeoutId: string | number | NodeJS.Timeout | undefined;
+    if (showConfetti) {
+      timeoutId = setTimeout(() => {
+        router.push("/lessons");
+      }, 5000);
+    }
+    return () => (timeoutId ? clearTimeout(timeoutId) : undefined);
+  }, [showConfetti, router]);
 
   if (vocabularies.length === 0) {
     return (
@@ -24,38 +47,59 @@ export default function VocabulariesSection({
       </div>
     );
   }
+
   return (
     <>
       <div className="mt-20 p-20 rounded-2xl bg-primary/5 relative">
-        <div className="absolute top-8 right-10">
-          {currentItem + 1} of {vocabularies.length} vocabularies
-        </div>
-        <ul className="space-y-6">
-          <li>
-            <h3 className="text-primary font-bold border-b-2 border-primary inline-block mb-2">
-              Vocabulary word :
-            </h3>
-            <p className="text-black">{vocabularies[currentItem].word}</p>
-          </li>
-          <li>
-            <h3 className="text-primary font-bold border-b-2 border-primary inline-block mb-2">
-              Pronunciation :
-            </h3>
-            <p>{vocabularies[currentItem].pronunciation}</p>
-          </li>
-          <li>
-            <h3 className="text-primary font-bold border-b-2 border-primary inline-block mb-2">
-              Meaning :
-            </h3>
-            <p>{vocabularies[currentItem].meaning}</p>
-          </li>
-          <li>
-            <h3 className="text-primary font-bold border-b-2 border-primary inline-block mb-2">
-              When to say :
-            </h3>
-            <p>{vocabularies[currentItem].whenToSay}</p>
-          </li>
-        </ul>
+        {showConfetti ? (
+          <div className="h-[300px] flex justify-center items-center">
+            <p className="text-xl font-bold">
+              Congratulations! 🎉 You&apos;ve successfully completed the lesson!
+            </p>
+          </div>
+        ) : (
+          <>
+            <div className="absolute top-8 right-10">
+              {currentItem + 1} of {vocabularies.length} vocabularies
+            </div>
+            <ul className="space-y-6">
+              <li>
+                <h3 className="text-primary font-bold border-b-2 border-primary inline-block mb-2">
+                  Vocabulary word :
+                </h3>
+                <p className="text-black flex items-center gap-x-2">
+                  {vocabularies[currentItem].word}{" "}
+                  <button
+                    onClick={() =>
+                      pronounceWord(vocabularies[currentItem].pronunciation)
+                    }
+                    className="group"
+                  >
+                    <Volume2 className="text-gray-500 group-hover:text-black transition duration-300" />
+                  </button>{" "}
+                </p>
+              </li>
+              <li>
+                <h3 className="text-primary font-bold border-b-2 border-primary inline-block mb-2">
+                  Pronunciation :
+                </h3>
+                <p>{vocabularies[currentItem].pronunciation}</p>
+              </li>
+              <li>
+                <h3 className="text-primary font-bold border-b-2 border-primary inline-block mb-2">
+                  Meaning :
+                </h3>
+                <p>{vocabularies[currentItem].meaning}</p>
+              </li>
+              <li>
+                <h3 className="text-primary font-bold border-b-2 border-primary inline-block mb-2">
+                  When to say :
+                </h3>
+                <p>{vocabularies[currentItem].whenToSay}</p>
+              </li>
+            </ul>
+          </>
+        )}
       </div>
       <div className="mt-10 flex justify-end gap-x-2">
         <Button
@@ -67,7 +111,9 @@ export default function VocabulariesSection({
           Previous
         </Button>
         {currentItem === vocabularies.length - 1 ? (
-          <Button size="lg">Complete</Button>
+          <Button size="lg" onClick={() => setShowConfetti(true)}>
+            Complete
+          </Button>
         ) : (
           <Button
             disabled={currentItem === vocabularies.length - 1}
@@ -78,6 +124,9 @@ export default function VocabulariesSection({
           </Button>
         )}
       </div>
+      {showConfetti ? (
+        <Confetti width={width - 100} height={height - 100} />
+      ) : null}
     </>
   );
 }
