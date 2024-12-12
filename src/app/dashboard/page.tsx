561efd54db1b8dd8c60960c0f9a8418147300767
rakib544 <md.rakib10122003@gmail.com>
2024-12-12 12:31:56 +0600

    feat:developed dashboard home page with some quick links and quotes

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,72 @@
+import { Testimonial } from "@/components/testimonial";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
+const quickLinks = [
+  {
+    title: "Manage Lessons",
+    link: "/dashboard/lessons",
+    description:
+      "View, edit, and organize all lessons available in the system.",
+  },
+  {
+    title: "Manage Tutorials",
+    link: "/dashboard/tutorials",
+    description:
+      "Add, update, or remove embedded YouTube tutorials for learning.",
+  },
+  {
+    title: "Manage Vocabularies",
+    link: "/dashboard/vocabularies",
+    description:
+      "Oversee, update, and add vocabulary entries linked to lessons.",
+  },
+  {
+    title: "Manage Users",
+    link: "/dashboard/users",
+    description: "Promote or demote users and manage their roles efficiently.",
+  },
+];
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h2 className="text-xl md:text-2xl font-bold text-foreground mb-3">
+        Welcome back, Rakib!
+      </h2>
+      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-12">
+        {quickLinks.map((link) => (
+          <li key={link.link}>
+            <Link
+              href={link.link}
+              className="grid h-full grid-cols-12 gap-x-2 custom-shadow p-5 rounded-lg border border-primary/5 group hover:border-primary/30 transition duration-300"
+            >
+              <div className="col-span-10">
+                <h3 className="text-lg font-bold">{link.title}</h3>
+                <p className="text-muted-foreground mt-1">{link.description}</p>
+              </div>
+              <div className="col-span-2 flex justify-end">
+                <span className="size-10 rounded-full border border-primary/10 flex justify-center items-center group-hover:border-primary transition duration-300">
+                  <ArrowRight className="-rotate-45 size-5 group-hover:text-primary transition duration-300" />
+                </span>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <div className="bg-slate-50 rounded-[40px] mt-20">
+        <Testimonial
+          author={{
+            name: "Dr. Emily Carter",
+            role: "Educator and Learning Strategist",
+          }}
+        >
+          True education is not just about the transfer of knowledge but about
+          inspiring curiosity and igniting a lifelong passion for learning. A
+          well-structured lesson acts as the cornerstone of this journey, laying
+          the groundwork for understanding and mastery in any field.
+        </Testimonial>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/testimonial.tsx
@@ -0,0 +1,33 @@
+import { Container } from "@/components/container";
+import { ReactNode } from "react";
+
+export function Testimonial({
+  author,
+  children,
+}: {
+  author: { name: string; role: string };
+  children?: ReactNode;
+}) {
+  return (
+    <aside
+      aria-label={`Testimonial from ${author.name}`}
+      className="relative py-16 sm:py-32 w-full"
+    >
+      <Container size="xs" className="relative">
+        <figure>
+          <blockquote className="mt-10 font-display text-xl lg:text-2xl font-medium tracking-tight text-slate-900 sm:text-center">
+            {children}
+          </blockquote>
+          <figcaption className="mt-10 flex items-center justify-center text-center">
+            <div>
+              <div className="text-base font-medium leading-6 tracking-tight text-slate-900">
+                {author.name}
+              </div>
+              <div className="mt-1 text-sm text-slate-600">{author.role}</div>
+            </div>
+          </figcaption>
+        </figure>
+      </Container>
+    </aside>
+  );
+}
